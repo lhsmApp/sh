85a812ef0a19aa3d7e98f5516927c586b249b82a
jiachao <jiaccool@163.com>
2017-07-13 08:31:31 +0800

    no message

--- src/main/java/com/fh/controller/houseFundDetail/housefunddetail/HouseFundDetailController.java
+++ src/main/java/com/fh/controller/houseFundDetail/housefunddetail/HouseFundDetailController.java
@@ -368,7 +368,7 @@ public class HouseFundDetailController extends BaseController {
 				} catch (Exception e) {
 					e.printStackTrace();
 					logger.error("读取Excel文件错误", e);
-					throw new CustomException("读取Excel文件错误");
+					throw new CustomException("读取Excel文件错误",false);
 				}
 				boolean judgement = false;
 				if(uploadAndReadMap.get(1).equals(false)){
